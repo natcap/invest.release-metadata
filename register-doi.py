16cc91bc1788ecc16012dac3277847f33d0290d9
James Douglass <jdouglass@stanford.edu>
2025-05-29 14:17:58 -0700

    Refactoring to update the DOI metadata if/when needed.

--- register-doi.py
+++ register-doi.py
@@ -8,6 +8,10 @@ import requests.exceptions
 TEST = '--test' in sys.argv
 PUBLISH = '--publish' in sys.argv
 AUTH_DATA = json.load(open('.env.json'))
+HEADERS = {
+    'accept': 'application/vnd.api+json',
+    "content-type": "application/json",
+}
 
 def register(datacite_json_path):
     datacite_json = clean_json.clean(json.load(open(datacite_json_path)))
@@ -21,16 +25,24 @@ def register(datacite_json_path):
         user = AUTH_DATA['datacite_user']
         prefix = "10.60793"
 
+    auth = tuple(user.split(':'))  # Hub, requests DEMANDS a tuple
+
     # there are different prefixes associated with prod and test accounts, so
     # set it appropriately here.
+    update = False
     try:
         doi = datacite_json['data']['attributes']['doi']
-        old_prefix, *suffixes = doi.split('/')
 
         # replace the prefix with our new prefix.
-        datacite_json['data']['attributes']['doi'] = (
-            f'{prefix}/{"/".join(suffixes)}')
-        print(datacite_json['data']['attributes']['doi'])
+        old_prefix, *suffixes = doi.split('/')
+        doi = f'{prefix}/{"/".join(suffixes)}'
+        datacite_json['data']['attributes']['doi'] = doi
+        print(doi)
+
+        # check to see if the DOI already exists; update if so
+        if requests.get(f'{endpoint}/dois/{doi}', headers=HEADERS, auth=auth):
+            print(f'DOI already exists: {doi}')
+            update = True
     except KeyError:
         datacite_json['data']['attributes']['prefix'] = prefix
 
@@ -39,14 +51,19 @@ def register(datacite_json_path):
 
     print(json.dumps(datacite_json, indent=4, sort_keys=True))
 
-    resp = requests.post(
-        f'{endpoint}/dois',
-        headers={
-            'accept': 'application/vnd.api+json',
-            "Content-Type": "application/json",
-        },
-        data=datacite_json,
-        auth=tuple(user.split(':')),  # Huh, requests VERY MUCH wants a tuple
+    if update:
+        request_method = requests.put
+        endpoint = f'{endpoint}/dois/{doi}'
+    else:
+        request_method = requests.post
+        endpoint = f'{endpoint}/dois'
+
+    print(request_method)
+    resp = request_method(
+        endpoint,
+        headers=HEADERS,
+        json=datacite_json,
+        auth=auth,
     )
     print(resp)
     try:
