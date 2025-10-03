42d28e9c0504b82898cc5bdba05abdb390cd36a5
James Douglass <jdouglass@stanford.edu>
2025-10-03 15:45:36 -0700

    Starting an argparse interface.

--- render_json.py
+++ render_json.py
@@ -7,8 +7,9 @@ Example usage:
     $ python render_json.py invest-releases/3.15.1/datacite.json context.json
 """
 
-
+import argparse
 import json
+import os
 import sys
 
 from jinja2 import Template
@@ -34,6 +35,49 @@ def render_with_context(source_json, context_json):
     with open(source_json) as source_file:
         template = Template(source_file.read())
         print(template.render(context))
+
+
+# Need way to provide extra context attributes
+# --context=year:2025
+# --context=version:3.16.3
+# --context=prefix:10.2139487o3241
+
+def main(args=None):
+    parser = argparse.ArgumentParser(
+        prog=os.path.basename(__file__),
+        description="Render a templated file based on defined attributes."
+    )
+    parser.add_argument('--variable', nargs='*', help=(
+        'A variable name and its value to be passed in to the template. '
+        'This can be provided multiple times. '
+        'For example, --variable=key:value --variable=year:2025. '
+        'No parsing is performed on the variable values.'
+    ))
+    parser.add_argument('--variable-json', nargs='?', help=(
+        'The path to a JSON file representing a dict.  Top-level keys '
+        'will be read as the variable names mapping to their values. '
+    ))
+
+    args = parser.parse_args(args)
+
+    variables = {}
+    if args.variable_json is not None:
+        with open(args.variable_json) as variable_json:
+            json_data = json.load(variable_json)
+        if not isinstance(json_data, dict):
+            raise ValueError(
+                f'The variable JSON file at {args.variable_json} must be a '
+                f'dict, not {type(json_data)}')
+        variables.update(json_data)
+
+    for variable in args.variable:
+        var_name, var_value = variable.split(':')
+        if var_name in variables:
+            raise ValueError(
+                f'The variable {var_name} is defined twice between CLI args '
+                'and the JSON file (if provided).')
+        variables[var_name] = var_value
+
 
 
 if __name__ == '__main__':
