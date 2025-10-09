9b1472731064a7d117f99e1bbe45a14296aeb59f
James Douglass <jdouglass@stanford.edu>
2025-10-08 18:11:56 -0700

    Continuing release script.

--- do-release.py
+++ do-release.py
@@ -1,15 +1,12 @@
 import json
 import logging
 import os
+import sys
+
+from jinja2 import Template
 
 logging.basicConfig(level=logging.WARNING)
 LOGGER = logging.getLogger(level=logging.INFO)
-
-# Always write out the non-test DOI prefix.
-# Create new datacite file (only overwrite with --overwrite)
-# Create new html file from datacite file
-# Recreate index file based on datacite files
-
 CWD = os.path.dirname(__file__)
 TEMPLATES_DIR = os.path.join(CWD, 'templates')
 RELEASES_DIR = os.path.join(CWD, 'invest-releases')
@@ -34,3 +31,39 @@ def get_versions_and_dates():
         data['date'] = datacite_json['dates']['date']
 
         version_data.append(data)
+
+
+def render_jinja(source_file: str, context: dict):
+    """Render a templated Jinja file given a dictionary.
+
+    The rendered file is printed to stdout.
+
+    Args:
+        source_file (str): The filepath to a file that includes jinja2 template
+            strings.
+        context (dict): A dictionary of keys mapping to values, where the keys
+            are used in the source file template.
+
+    Returns:
+        ``rendered_text``, a string.
+    """
+    with open(source_file) as source_fp:
+        template = Template(source_fp.read())
+    rendered_text = template.render(context)
+    return rendered_text
+
+
+def main(args=None):
+    # Args: version
+    # Args: publication date (ISO)
+    # Args: --overwrite
+
+    # Write new datacite file
+    # Write new index file
+    # Write new release html file
+
+    pass
+
+
+if __name__ == '__main__':
+    main(sys.argv)
