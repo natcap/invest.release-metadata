c919eddb8652e53a5682afd89b591c08988b524c
James Douglass <jdouglass@stanford.edu>
2025-10-08 18:31:42 -0700

    Adding the hard work of rendering templates.

--- do-release.py
+++ do-release.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import logging
 import os
@@ -10,6 +11,7 @@ LOGGER = logging.getLogger(level=logging.INFO)
 CWD = os.path.dirname(__file__)
 TEMPLATES_DIR = os.path.join(CWD, 'templates')
 RELEASES_DIR = os.path.join(CWD, 'invest-releases')
+HTML_DIR = os.path.join(CWD, 'html')
 
 
 def get_versions_and_dates():
@@ -58,11 +60,44 @@ def main(args=None):
     # Args: publication date (ISO)
     # Args: --overwrite
 
-    # Write new datacite file
-    # Write new index file
-    # Write new release html file
+    version = '3.17.0'
+    publication_date = '2025-08-09'
+    overwrite = True
 
-    pass
+    # Fails if the date format isn't in ISO
+    date = datetime.date.fromisoformat(publication_date)
+
+    version = version.strip()  # to remove any whitespace.
+
+    release_dir = os.path.join(RELEASES_DIR, version)
+    if not os.path.exists(release_dir):
+        os.makedirs(release_dir)
+
+    target_datacite = os.path.join(release_dir, 'datacite.json')
+    target_index_html = os.path.join(HTML_DIR, 'index.html')
+    target_release_html = os.path.join(HTML_DIR, f'{version}.html')
+
+    # we have the same kinds of variable names across files and it's not a
+    # problem to have keys here that aren't used by a given template.
+    template_data = {
+        'versions': get_versions_and_dates() + [
+            {'version': version, 'date': publication_date}],
+        'version': version,
+        'year': str(date.year),
+        'date': date.strftime('%Y-%m-%d'),
+    }
+
+    # template, target
+    files_to_process = [
+            ('datacite.json.template', target_datacite),
+            ('index.html.template', target_index_html),
+            ('release.html.template', target_release_html),
+    ]
+    for template_file, target_file in files_to_process:
+        LOGGER.info(f'Rendering template to {target_file}')
+        template_file = os.path.join(TEMPLATES_DIR, template_file)
+        with open(target_file, 'w') as target_fp:
+            target_fp.write(render_jinja(template_file, template_data))
 
 
 if __name__ == '__main__':
